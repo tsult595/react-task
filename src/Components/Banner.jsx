3921ba528c040590701ecded4779f9f3c7fb685a
tsult595 <teymurt@hotmail.com>
2025-05-18 04:23:47 -0700

    task id done

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from './Button'
 
 const Banner = () => {
+  const [showLess, setShowLess] = useState(false)
+
+  function toggleShow() {
+    setShowLess(prev => !prev)
+  }
+
   return (
-    <div className='banner'>We Keep Good Company</div>
+    <>
+      <div className='banner'>We Keep Good Company</div>
+
+      <div className={`banner ${showLess ? 'banner-max' : 'banner-min'}`}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          doloremque numquam natus eaque praesentium maxime perspiciatis
+          exercitationem, ad quae accusantium distinctio necessitatibus
+          voluptate nemo fugit Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Assumenda ab rem voluptatibus fugiat officiis option
+          voluptas facere incidunt quibusdam accusantium?.
+        </p>
+        
+      </div>
+      <div className='showsBtn'>
+        <Button
+          onClick={toggleShow}
+          className='showBtn'
+          text={`${showLess ? 'Show Less' : 'Show More'}`}
+        />
+      </div>
+    </>
   )
 }
 
